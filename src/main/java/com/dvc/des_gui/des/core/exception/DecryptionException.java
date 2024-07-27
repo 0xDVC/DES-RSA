8eb8e2d211e86198135a7eba20d0ecb76f43acfd
dvc-77 <neilohene@gmail.com>
2024-07-27 11:16:31 +0000

    chore: remove custom exception

--- src/main/java/com/dvc/des_gui/des/core/exception/DecryptionException.java
+++
@@ -1,7 +0,0 @@
-package com.dvc.des_gui.des.core.exception;
-
-public class DecryptionException extends Exception{
-    public DecryptionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/com/dvc/des_gui/des/core/exception/EncryptionException.java
+++
@@ -1,7 +0,0 @@
-package com.dvc.des_gui.des.core.exception;
-
-public class EncryptionException extends Exception {
-    public EncryptionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
